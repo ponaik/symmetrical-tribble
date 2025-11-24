f34edf7154b74ee25e56433115c303040d14b68c
Ioann Yaromenka <ioann.yaromenka@innowise.com>
2025-11-24 18:56:35 +0300

    External api sketch

---
+++ src/main/java/com/intern/paymentservice/client/DecisionClient.java
@@ -0,0 +1,26 @@
+package com.intern.paymentservice.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class DecisionClient {
+
+
+    private final WebClient webClient;
+
+    public DecisionClient(
+            @Value("${server.port}") String port,
+            WebClient.Builder builder) {
+        this.webClient = builder.baseUrl("http://localhost:" + port).build();
+    }
+
+    public Integer getPaymentDecision() {
+        return webClient.get()
+                .uri("/totallyLegitDecisionApi")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(Integer.class)
+                .block();
+    }
+}
